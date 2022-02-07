b19235f46b2c1cf228cbed966ba8954309966182
Mariya <masa995@yandex.ru>
2022-02-07 03:34:23 +0300

    updated menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 const btnMenu = document.querySelector('.js-btns-menu__icon');
 const listMenu = document.querySelector('.js-btns-menu__list');
 
-btnMenu.addEventListener('click', () => {
-  btnMenu.classList.toggle('active');
-  listMenu.classList.toggle('active');
-});
+if (btnMenu) {
+  btnMenu.addEventListener('click', () => {
+    btnMenu.classList.toggle('active');
+    listMenu.classList.toggle('active');
+  });
+}
 
 function isTouchDevice(e) {
   return typeof window.ontouchstart !== 'undefined';
